55ba437d5649ae709527f7cee80a4651982dbd16
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-06-28 14:55:01 +0200

    created main project

---
+++ Main/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Main
+{
+    #region usings
+
+    using System;
+    using System.Windows.Forms;
+
+    #endregion
+
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
