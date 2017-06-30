1c46d3c54b68913b8c9bae76c1bc62e335fbf968
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-06-30 17:06:17 +0200

    added plugin unzip project

---
+++ Plugins/PluginUnzip/PluginUnzip/Plugin.cs
@@ -0,0 +1,28 @@
+﻿namespace ch.wuerth.tobias.filehandler.PluginUnzip
+{
+    #region usings
+
+    using System;
+    using System.Collections.Generic;
+
+    using filehandler.Plugin;
+
+    #endregion
+
+    public class Plugin : PluginBase
+    {
+        public Plugin()
+        {
+            Name = "UnZip";
+            Description = "Unzip .zip archives";
+            Extensions = new List<String>
+                         {
+                             ".zip"
+                         };
+            VersionMajor = 1; // Last changed 2017-06-30 T 1705
+            VersionMinor = 1; // Last changed 2017-06-30 T 1705
+        }
+
+        public override void Action(String path) { }
+    }
+}
